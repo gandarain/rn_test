3664d67a2a9ba1c2e8d687f5a23f2c17ae0f006f
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2021-04-07 05:49:35 +0700

    setup font

--- src/views/homeScreen/index.js
+++ src/views/homeScreen/index.js
@@ -6,7 +6,7 @@ const HomeScreen = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Icon name="rocket" size={30} color="#900" />
-      <Text>Home Screen</Text>
+      <Text style={{fontSize: 20, fontFamily: 'OpenSans-Bold'}}>Home Screen</Text>
     </View>
   )
 }
